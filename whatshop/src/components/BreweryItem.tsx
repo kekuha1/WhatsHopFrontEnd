934bf0e6578bc4da492bc602f451fc3e14e6e4e8
David <isaacsdavid28@gmail.com>
2023-03-19 15:22:52 -0400

    first commit

--- whatshop/src/components/BreweryItem.tsx
+++ whatshop/src/components/BreweryItem.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Brewery from '../Model/Brewery';
+import { Button, Card, CardBody, CardDeck, CardSubtitle, CardText, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-type Props = {}
+export interface IBreweryItemProps {
+  brewery: Brewery
+}
 
-export default function BreweryItem({}: Props) {
+export function BreweryItem(props: IBreweryItemProps) {
+  let {brewery} = props;
+
+// use context here to create the addBrewery function to add something to the favorites list
+
   return (
-    <div>BreweryItem</div>
+    <CardDeck>
+      <Card>
+        <CardBody>
+          <CardTitle tag="h5">{brewery.name}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted">
+            {brewery.city},{brewery.state}
+          </CardSubtitle>
+          <CardText>{brewery.brewery_type}</CardText>
+          <Link to={`/reviewsroute/${brewery.id}`}>Reviews</Link>
+          {/* add onClick function */}
+          <Button className="AddBrewery">
+            Add to Favorites
+            </Button>  
+        </CardBody>
+      </Card>
+    </CardDeck>
   )
 }
--- whatshop/src/components/BreweryList.tsx
+++ whatshop/src/components/BreweryList.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Brewery from '../Model/Brewery'
+import SearchForm from './SearchForm'
+import { Col, Row } from 'reactstrap'
+import { BreweryItem } from './BreweryItem'
 
-type Props = {}
+export function BreweryList() {
 
-export default function BreweryList({}: Props) {
+  const [brewerys, setBrewerys] = useState<Brewery[]>()
+
+  //useEffect hook to get the 12 breweries on page load
+
+  //useEffect hook to filter the breweries from the SearchForm
+  function filterBrewerys(brewerys:Brewery[]){
+    setBrewerys(brewerys)
+  }
+
   return (
-    <div>BreweryList</div>
+    <div className='Brewerys'>
+      {/* <SearchForm filterBrewerys={filterBrewerys}/> */}
+      <Row>
+        { brewerys?.length ?
+          brewerys?.map((brewery) => (
+            <Col lg="4" key={brewery.id}>
+                <BreweryItem brewery={brewery} />
+            </Col>
+          )) || ""
+        : <Col tag="h1">No breweries found for your search terms, please try again</Col>
+        }
+      </Row>
+    </div>
   )
 }
