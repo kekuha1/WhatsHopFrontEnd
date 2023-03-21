55ec89944c42114586f2eb229fc4de86ff16974d
David <isaacsdavid28@gmail.com>
2023-03-20 20:19:18 -0400

    data to screen

--- whatshop/src/components/BreweryList.tsx
+++ whatshop/src/components/BreweryList.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Brewery from '../Model/Brewery'
-import SearchForm from './SearchForm'
 import { Col, Row } from 'reactstrap'
 import { BreweryItem } from './BreweryItem'
 import axios from 'axios'
@@ -11,14 +10,20 @@ export function BreweryList() {
 
   //useEffect hook to get the 12 breweries on page load
  useEffect(() => {
-    axios.get<Brewery[]>('/breweries')
+   axios.get<Brewery[]>('http://127.0.0.1:5001/whatshop-b56f0/us-central1/getBreweries/breweries') 
       .then(response => setBreweries(response.data))
       .catch(error => console.error(error));
   }, []);
+
+  // useEffect(() => {
+  //  axios.get<Brewery[]>('https://whatshop-b56f0.web.app//breweries') 
+  //     .then(response => setBreweries(response.data))
+  //     .catch(error => console.error(error));
+  // }, []);
   //useEffect hook to filter the breweries from the SearchForm
-  function filterBrewerys(brewerys:Brewery[]){
-    setBreweries(breweries)
-  }
+  // function filterBrewerys(brewerys:Brewery[]){
+  //   setBreweries(breweries)
+  // }
 
   return (
     <div className='Brewerys'>
--- whatshop/src/context/BreweryContextModel.ts
+++
@@ -1,8 +0,0 @@
-import Brewery from '../Model/Brewery';
-
-export interface BreweryContextModel {
-  brewery:Brewery[],
-  addBrewery:(brewery:Brewery) => void,
-  removeBrewery:(id:string) => void,
-}
-
