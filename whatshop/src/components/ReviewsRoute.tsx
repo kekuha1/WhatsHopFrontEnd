42b6763fd2305b1b265603ada457e5443ca21862
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-03-26 15:07:33 -0400

    updated url

--- whatshop/src/components/ReviewsRoute.tsx
+++ whatshop/src/components/ReviewsRoute.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import { useParams } from 'react-router-dom';
-import Review from '../model/Review'
-import { useEffect, useState } from 'react';
-import { ReviewsList } from './ReviewsList';
+import * as React from "react";
+import { useParams } from "react-router-dom";
+import Review from "../model/Review";
+import { useEffect, useState } from "react";
+import { ReviewsList } from "./ReviewsList";
 // import { fetchReviews } from '../services/ReviewServices';
-import ReviewForm from './ReviewForm';
-import { fetchReviews } from '../services/ReviewServices';
-
+import ReviewForm from "./ReviewForm";
+import { fetchReviews } from "../services/ReviewServices";
 
 function ReviewsRoute() {
-  const { id, name } = useParams<{ id: string, name: string }>();
-  
-  const [breweryId, setBreweryId] = useState<string>('');
+  const { id, name } = useParams<{ id: string; name: string }>();
+
+  const [breweryId, setBreweryId] = useState<string>("");
   const [reviews, setReviews] = useState<Review[]>([]);
 
   useEffect(() => {
@@ -27,13 +26,16 @@ function ReviewsRoute() {
   }, [id, fetchReviews]);
 
   return (
-    <div className='ReviewsRoute'>
+    <div className="ReviewsRoute">
       <h1>Reviews for {name}</h1>
-      {reviews.length > 0 ? <ReviewsList reviews={reviews} /> : <p>Loading reviews...</p>}
+      {reviews.length > 0 ? (
+        <ReviewsList reviews={reviews} />
+      ) : (
+        <p>Loading reviews...</p>
+      )}
       <ReviewForm brewery_id={breweryId} />
     </div>
   );
 }
 
 export default ReviewsRoute;
-
--- whatshop/src/services/ReviewServices.ts
+++ whatshop/src/services/ReviewServices.ts
@@ -1,14 +1,18 @@
-import axios from 'axios';
-import Review from '../model/Review';
+import axios from "axios";
+import Review from "../model/Review";
 
+const baseUrl = process.env.REACT_APP_BASE_URL || "";
 
-const baseUrl = process.env.REACT_APP_BASE_URL || ""
+export async function fetchReviews(): Promise<Review[]> {
+  console.log(baseUrl);
+  return await axios
 
-export async function fetchReviews():Promise<Review[]> {
-    return await axios.get<Review[]>(`${baseUrl}/reviews`)
-    .then((res) => res.data)
+    .get<Review[]>(`${baseUrl}/reviews`)
+    .then((res) => res.data);
 }
 
-export async function addReview (review:Review):Promise<Review> {
-    return await axios.post<Review>(`${baseUrl}/reviews`, review).then((res) => res.data)
+export async function addReview(review: Review): Promise<Review> {
+  return await axios
+    .post<Review>(`${baseUrl}/reviews`, review)
+    .then((res) => res.data);
 }
