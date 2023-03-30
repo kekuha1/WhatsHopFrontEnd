8b4b48f2b240572d14c213f37e655842b5e57c28
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2023-03-30 19:12:48 -0400

    master

--- whatshop/src/services/userservices.ts
+++ whatshop/src/services/userservices.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { User } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { MongoClient } from "mongodb";
 
 const baseUrl = process.env.REACT_APP_BASE_URL || ""
 export async function GetAllUsers(): Promise<any[]> {
@@ -6,6 +9,30 @@ export async function GetAllUsers(): Promise<any[]> {
   }
 
 // getProfile () - get profile by id
-
+export async function getProfile(userId: string): Promise<User> {
+  const db = getFirestore();
+  const userRef = doc(db, "users", userId);
+  const userDoc = await getDoc(userRef);
+  if (userDoc.exists()) {
+    return userDoc.data() as User;
+  } else {
+    throw new Error("User not found");
+  }
+}
 
 //make profile () - creates a profile send to mongodb
+const mongoUrl = "mongodb://localhost:3000";
+const dbName = "myDatabase";
+const collectionName = "users";
+
+export async function makeProfile(user: User): Promise<void> {
+  const client = new MongoClient(mongoUrl);
+  try {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    await collection.insertOne(user);
+  } finally {
+    await client.close();
+  }
+}
