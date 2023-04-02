20fc836a6cbd3e8715d7c6f6294668f36f99f0c5
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-04-02 15:18:41 -0400

    added header font

--- whatshop/src/components/Header.tsx
+++ whatshop/src/components/Header.tsx
@@ -17,9 +17,9 @@ export function Header() {
       <li>
          <Link className='headerlinkbutton' to="/glossary">Glossary</Link>
          </li>
-       <li>
+       {/* <li>
          <Link className='headerlinkbutton' to="/profile">Profile</Link>
-       </li>
+       </li> */}
        {/* <li>
          <Link className='headerlink' to="/my-reviews">My Reviews</Link>
        </li> */}
