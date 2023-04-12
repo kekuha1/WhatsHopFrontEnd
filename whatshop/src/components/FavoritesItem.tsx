9c3f2c53ca43852eaafb0da6331d1a8fd7ebf950
David <isaacsdavid28@gmail.com>
2023-04-11 21:25:37 -0400

    Not enough badges to train me

--- whatshop/src/components/FavoritesItem.tsx
+++ whatshop/src/components/FavoritesItem.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from "reactstrap";
 import { deleteFavorite } from "../services/favoritesservice";
 import  Favorites  from "../model/Favorites";
+import { Link } from "react-router-dom";
 
 interface IFavoritesItemProps {
   favorite: Favorites;
@@ -19,9 +20,9 @@ export function FavoritesItem({ favorite, onDelete }: IFavoritesItemProps) {
         <CardTitle tag="h5">{favorite.brewery.name}</CardTitle>
         <CardSubtitle>{favorite.brewery.city}, {favorite.brewery.state}</CardSubtitle>
         <CardText>{favorite.brewery.brewery_type}</CardText>
-        <Button color="primary">Reviews</Button>
-        <Button color="primary">Details</Button>
-        <Button color="danger" onClick={handleDelete}>Remove</Button>
+        <Link to={`/reviews/${favorite.brewery.id}?name=${favorite.brewery.name}`}><button className="CardReviews">Reviews</button></Link>
+      <Link to={`/brewerydetail/${favorite.brewery.id}`}><button className="CardDetails">Details</button></Link>
+        <Button color="danger" onClick={handleDelete}><i className="fa fa-trash">Remove</i></Button>
       </CardBody>
     </Card>
   );
